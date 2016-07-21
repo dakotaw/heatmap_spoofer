66a2126a604d6f7990ad4c94667aee52f4362b79
Dakota Williams <dakota@sloot.local>
2016-07-21 17:55:03 -0400

    testing commit.

--- spoofer.py
+++ spoofer.py
@@ -1,3 +1,21 @@
 #!/usr/local/python
 
-import git
+from git import Repo
+from os import getcwd
+from random import randint
+import random_wiki
+
+repo = Repo(getwcd())
+
+# We want between 0 - 10 commits, but want to weight it on the lighter side;
+# maybe slightly more realistic this way?
+num_commits = randint(0, 5)
+
+while num_commits:
+
+	# Get a random page from Wikipedia and write it to a markdown file. 
+	random_page = random_wiki.get_random_page()
+	random_wiki.write_page(random_page)
+	repo.git.commit(m='message')
+
+	num_commits -= 1
